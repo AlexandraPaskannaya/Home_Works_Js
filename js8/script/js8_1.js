87be090c4b665decc67159aff7c5b6d67dc8328f
AlexandraPaskannaya <paskannaya1998@yandex.by>
2020-12-08 16:11:20 +0300

    решение дз 8

--- js8/script/js8_1.js
+++ js8/script/js8_1.js
@@ -1,15 +1,15 @@
 /*- Дан объект let user = { name: "Ivan", years: 19, car: "Subaru", telePhone: "iPhone", city: "Minsk" }.
-   Запишите поле name в переменную userName, поле year в переменную userName, поле car в переменную userCar, остальные поля
+   Запишите поле name в переменную userName, поле year в переменную userYear, поле car в переменную userCar, остальные поля
      записать в объект userOtherInfo;*/
     
 let user = { name: "Ivan", 
-     years: 19, 
+     year: 19, 
      car: "Subaru", 
      telePhone: "iPhone", 
      city: "Minsk"
 };
 
-let {name: userName, car: userCar, ...userOtherInfo} = user;
-console.log(userName, userCar, userOtherInfo);
+let {name: userName, year: UserYaers, car: userCar, ...userOtherInfo} = user;
+console.log(userName, UserYaers, userCar, userOtherInfo);
 
 
