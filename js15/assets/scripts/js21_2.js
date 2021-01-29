96e932590ee30b7888da1bf52a61cd42f3e64314
AlexandraPaskannaya <72980696+AlexandraPaskannaya@users.noreply.github.com>
2021-01-29 23:44:16 +0300

    Update js21_2.js

--- js15/assets/scripts/js21_2.js
+++ js15/assets/scripts/js21_2.js
@@ -2,14 +2,22 @@
 	  Ваша задача написать функцию countRepeats которая будет выводить информацию о том какой элемент в данном массиве сколько раз повторяется.
       Функция должна возвращать объект.*/
       
-let  result =  ["str", "str1", "str2", "str", "str2", "str", "str3", "str1", "str4", "str4"].reduce(function(countRepeats, elem) {
-    countRepeats[elem] = (countRepeats[elem] || 0) + 1;
-    return countRepeats;
-}, {});
+let result = ["str", "str1", "str2", "str", "str2", "str", "str3", "str1", "str4", "str4"];
 
-console.log(result);
+function countRepeats() {
 
-/*result.sort(function (a,b) {
-    return a-b;
-})*/
+let array = {};
+    result.forEach(function(i){
+        if (array[i] != undefined)
+         ++array[i];
+        else
+         array[i] = 1;
+    });
+let obdj = Object.entries(array).map(([ key, val ]) => ({key, val})).sort((a, b) => b.val - a.val);
+      
+let newObdj = Object.assign({}, obdj);
+
+console.log(newObdj)
+}
+countRepeats()
 
